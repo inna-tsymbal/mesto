0319eec730b7743e2bfbee38dc59bec32ec45140
Inna Tsymbal <inna-tsymbal2001@yandex.ru>
2023-01-17 15:11:11 +0300

    первая попытка

---
+++ index.js
@@ -0,0 +1,33 @@
+// обозначила все нужные переменные 
+let popupElement = document.querySelector('.popup');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = popupElement.querySelector('.popup__close-button');
+let formElement = popupElement.querySelector('.popup__field');
+let popupSaveButton = formElement.querySelector('.popup__save-button');
+let nameInput = formElement.querySelector('.popup__person_name');
+let professionInput = formElement.querySelector('.popup__person_profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
+
+// Открытие попапа
+let popupOpen = function(){
+    popupElement.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    professionInput.value = profileProfession.textContent;
+}
+popupOpenButton.addEventListener('click', popupOpen);
+
+// Закрытие попапа
+let popupClose = function(){
+    popupElement.classList.remove('popup_opened');
+}
+popupCloseButton.addEventListener('click', popupClose);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = professionInput.value;
+    popupClose();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
