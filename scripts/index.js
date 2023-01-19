03e47ed54f28af8d5eede2cd9734409a86da079d
Inna Tsymbal <inna-tsymbal2001@yandex.ru>
2023-01-19 14:54:38 +0300

    вторая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -3,15 +3,15 @@ let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popupElement.querySelector('.popup__close-button');
 let formElement = popupElement.querySelector('.popup__field');
 let popupSaveButton = formElement.querySelector('.popup__save-button');
-let nameInput = formElement.querySelector('.popup__person_name');
-let professionInput = formElement.querySelector('.popup__person_profession');
+let nameInput = formElement.querySelector('.popup__person_about_name');
+let JobInput = formElement.querySelector('.popup__person_about_job');
 let profileName = document.querySelector('.profile__name');
-let profileProfession = document.querySelector('.profile__profession');
+let profileJob = document.querySelector('.profile__job');
 
 let popupOpen = function(){
     popupElement.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
-    professionInput.value = profileProfession.textContent;
+    JobInput.value = profileJob.textContent;
 }
 popupOpenButton.addEventListener('click', popupOpen);
 
@@ -23,7 +23,7 @@ popupCloseButton.addEventListener('click', popupClose);
 function handleFormSubmit (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
-    profileProfession.textContent = professionInput.value;
+    profileJob.textContent = JobInput.value;
     popupClose();
 }
 
