bd4cd6b730f5150d169fd5c7a7e842beafe98b3e
Inna Tsymbal <inna-tsymbal2001@yandex.ru>
2023-03-27 15:55:58 +0300

    исправлен popup

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -6,21 +6,20 @@ export default class Popup {
 
   open() {
     this._popup.classList.add("popup_opened");
-    this._handleEscClose();
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove("popup_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
   }
 
-  _handleEscClose() {
-    document.addEventListener("keydown", (evt) => {
-      if (evt.key === "Escape") {
-        this.close();
-      }
-    });
+  _handleEscClose = (evt) => {
+    if (evt.key === "Escape") {
+      this.close();
+    }
   }
-
+  
   setEventListeners() {
     this._popup.addEventListener("mousedown", (evt) => {
       if (evt.target.classList.contains("popup_opened")) {
